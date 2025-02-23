9f58b7867f0e2ef0b2fc737dbfb292261e6042cd
Laelmano24 <brockbomba@gmail.com>
2025-02-23 10:40:46 -0300

    update test.lua

--- Universal/test.lua
+++ Universal/test.lua
@@ -1,6 +1,6 @@
 local infomation = {
   
-  "https://i.postimg.cc/tgWg5R5s/9bacbd053fd1ae07faa9a5557d3cf5b4.gif",
+  "https://i.postimg.cc/T1krdHKM/lv-0-20250223100607.gif",
   "https://i.postimg.cc/SRw5D4VK/e282424b2847885602230435e6aff1cc.gif",
   "https://i.postimg.cc/nMbRqkcj/65c890782049f23f6548f4e948c0b87a.gif"
   
