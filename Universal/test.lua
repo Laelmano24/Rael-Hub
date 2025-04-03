c05a708f2a724644e439cfca3561f57769f0e47a
Laelmano24 <brockbomba@gmail.com>
2025-04-03 10:34:45 -0300

    update test.lua

--- Universal/test.lua
+++ Universal/test.lua
@@ -1,9 +1,1 @@
-local infomation = {
-  
-  "https://i.postimg.cc/T1krdHKM/lv-0-20250223100607.gif",
-  "https://i.postimg.cc/SRw5D4VK/e282424b2847885602230435e6aff1cc.gif",
-  "https://i.postimg.cc/nMbRqkcj/65c890782049f23f6548f4e948c0b87a.gif"
-  
-}
-
-return infomation
+return "https://i.postimg.cc/ncVbFQRF/lv-0-20250403103225.gif"
