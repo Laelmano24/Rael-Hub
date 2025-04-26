b2f078e39504ef9aedfcdfe29626963a6daabae8
Laelmano24 <brockbomba@gmail.com>
2025-04-26 12:47:48 -0300

    update

--- DOORS/main.lua
+++ DOORS/main.lua
@@ -2,4 +2,4 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local GameData = ReplicatedStorage.GameData
 local FloorName = GameData.Floor.Value
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/raelhubfunctions/Rael-Hub-functions/refs/heads/main/Rael%20Translation%20API/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Laelmano24/Rael-Hub/refs/heads/main/DOORS/floors/Doors%20" .. FloorName.. ".txt"))()
